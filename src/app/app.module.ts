379a2919f4a1330a1c6a3fbf4b6b839186180118
TH <kishu@webtoonscorp.com>
2019-01-01 13:47:43 +0900

    refactor(app): module definition list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared.module';
-import { AuthGuard } from './shared/guards';
-import { UserResolver } from './shared/resolvers';
-import { AuthModule } from './modules/auth/auth.module';
-import { GoodsModule } from './modules/goods/goods.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// firebase modules
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
-import { environment } from '../environments/environment';
+// applications modules
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { GoodsModule } from './modules/goods/goods.module';
+import { AppRoutingModule } from './app-routing.module';
+// providers
+import { AuthGuard } from './shared/guards';
+import { UserResolver } from './shared/resolvers';
+// components
+import { AppComponent } from './app.component';
 import { IntroComponent } from './modules/intro/intro.component';
 import { HomeComponent } from './modules/home/home.component';
-import { CloudinaryModule } from '@cloudinary/angular-5.x';
-import * as  Cloudinary from 'cloudinary-core';
+// environment
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -27,15 +30,14 @@ import * as  Cloudinary from 'cloudinary-core';
   imports: [
     BrowserModule,
     HttpClientModule,
-    SharedModule,
-    AuthModule,
-    GoodsModule,
-    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireStorageModule,
-    CloudinaryModule.forRoot(Cloudinary, { cloud_name: 'dhtyfa1la'}),
+    SharedModule,
+    AuthModule,
+    GoodsModule,
+    AppRoutingModule
   ],
   providers: [
     AuthGuard,
