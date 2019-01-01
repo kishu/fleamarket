5a83d4b1af2d54d52949440d5b406491af147f64
TH <kishu@webtoonscorp.com>
2019-01-01 16:15:43 +0900

    feat(group): create group service

---
+++ src/app/core/http/group.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { first, map } from 'rxjs/operators';
+import { Group, GroupType } from '../../shared/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupService {
+  private groupCollection: AngularFirestoreCollection<Group>;
+
+  constructor(private readonly afs: AngularFirestore) {
+    this.groupCollection = afs.collection<Group>('groups');
+  }
+
+  getGroupsByType(type: GroupType) {
+    return this.afs.collection('groups', ref => {
+      return ref.where('type', '==', type)
+        .orderBy('name', 'asc');
+    }).snapshotChanges().pipe(
+      first(),
+      map(groups => {
+        return groups.map(group => {
+          return {
+            id: group.payload.doc.id,
+            ...group.payload.doc.data()
+          } as Group;
+        });
+      })
+    );
+  }
+}
--- src/app/core/http/index.ts
+++ src/app/core/http/index.ts
@@ -1,3 +1,5 @@
 export * from './auth.service';
+// todo remove corp service
 export * from './corp.service';
+export * from './group.service';
 export * from './user.service';
