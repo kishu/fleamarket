fb2f3bb6aedd8c8de325bb0e4595f2a21f0298cf
TH <kishu@navercorp.com>
2019-01-20 21:03:51 +0900

    fixed(goods/write): price max length

--- src/app/modules/goods/write/write.component.ts
+++ src/app/modules/goods/write/write.component.ts
@@ -77,7 +77,7 @@ export class WriteComponent implements OnInit {
       ],
       price: [
         '',
-        [ Validators.required, Validators.maxLength(13) ]
+        [ Validators.required, Validators.maxLength(15) ]
       ],
       delivery: this.fb.group({
         delivery: '직거래',
