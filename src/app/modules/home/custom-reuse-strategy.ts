ca6e500b0ffb604974b6b0bd06d0ede1f4fb6f90
TH <kishu@navercorp.com>
2019-01-25 17:25:16 +0900

    wip: reuse strategy debugging

--- src/app/modules/home/custom-reuse-strategy.ts
+++ src/app/modules/home/custom-reuse-strategy.ts
@@ -6,41 +6,39 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
   constructor() { }
 
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    console.debug('shouldDetach',
-      route && JSON.stringify(route.routeConfig.path),
-      route && route.routeConfig.path === '');
+    // console.debug('shouldDetach',
+    //   route && JSON.stringify(route.routeConfig.path),
+    //   route && route.routeConfig.path === '');
     return route && route.routeConfig.path === '';
   }
 
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    console.debug('store',
-      route && JSON.stringify(route.routeConfig.path),
-      handle);
+    // console.debug('store',
+    //   route && JSON.stringify(route.routeConfig.path),
+    //   handle);
     this.storedHandle = handle;
   }
 
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    console.debug('shouldAttach',
-      route && JSON.stringify(route.routeConfig.path),
-      route && route.routeConfig.path === '' && !!this.storedHandle);
+    // console.debug('shouldAttach',
+    //   route && JSON.stringify(route.routeConfig.path),
+    //   route && route.routeConfig.path === '' && !!this.storedHandle);
     return route && route.routeConfig.path === '' && !!this.storedHandle;
   }
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    console.debug('retrieve',
-      route && route.routeConfig && JSON.stringify(route.routeConfig.path),
-      this.storedHandle);
+    // console.debug('retrieve',
+    //   route && route.routeConfig && JSON.stringify(route.routeConfig.path),
+    //   this.storedHandle);
     return this.storedHandle;
   }
 
   shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    if ( (future && future.routeConfig && curr && curr.routeConfig) &&
-      (curr.routeConfig.path !== ':market/goods/:goodsId') &&
-      (future.routeConfig.path === '') ) {
-      console.debug('shouldReuseRoute', 'set null');
-      this.storedHandle = null;
+    if ( (future && future.routeConfig && curr && curr.routeConfig) ) {
+      if ( (curr.routeConfig.path !== ':market/goods/:goodsId') && (future.routeConfig.path === '') ) {
+        this.storedHandle = null;
+      }
     }
-
     return future.routeConfig === curr.routeConfig;
   }
 
