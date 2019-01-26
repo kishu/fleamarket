c7d3d9b84d825e42b735b08adfd23bad095c2e4e
TH <kishu@navercorp.com>
2019-01-26 12:00:44 +0900

    refactor(reuse-strategy): reuse strategy

--- src/app/modules/home/custom-reuse-strategy.ts
+++ src/app/modules/home/custom-reuse-strategy.ts
@@ -5,41 +5,74 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
 
   constructor() { }
 
-  shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    // console.debug('shouldDetach',
-    //   route && JSON.stringify(route.routeConfig.path),
-    //   route && route.routeConfig.path === '');
-    return route && route.routeConfig.path === '';
-  }
-
-  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    // console.debug('store',
-    //   route && JSON.stringify(route.routeConfig.path),
-    //   handle);
-    this.storedHandle = handle;
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    // console.group('shouldReuseRoute');
+    // console.log(future, curr);
+    // if (future && future.routeConfig) {
+    //   console.log(JSON.stringify(future.routeConfig.path));
+    // }
+    // if (curr && curr.routeConfig) {
+    //   console.log(JSON.stringify(curr.routeConfig.path));
+    // }
+    // console.log('return', future.routeConfig === curr.routeConfig);
+    // console.groupEnd();
+    return future.routeConfig === curr.routeConfig;
   }
 
   shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    // console.debug('shouldAttach',
-    //   route && JSON.stringify(route.routeConfig.path),
-    //   route && route.routeConfig.path === '' && !!this.storedHandle);
+    // console.group('shouldAttach');
+    // console.log(route);
+    // if (route && route.routeConfig) {
+    //   console.log('path', JSON.stringify(route.routeConfig.path));
+    //   console.log('storedHandle', this.storedHandle);
+    // }
+    // console.log('return', route.routeConfig.path === '' && !!this.storedHandle);
+    // console.groupEnd();
     return route && route.routeConfig.path === '' && !!this.storedHandle;
   }
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    // console.debug('retrieve',
-    //   route && route.routeConfig && JSON.stringify(route.routeConfig.path),
-    //   this.storedHandle);
-    return this.storedHandle;
-  }
-
-  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    if ( (future && future.routeConfig && curr && curr.routeConfig) ) {
-      if ( (curr.routeConfig.path !== ':market/goods/:goodsId') && (future.routeConfig.path === '') ) {
-        this.storedHandle = null;
+    // console.group('retrieve');
+    // console.log(route);
+    if (route && route.routeConfig) {
+      // console.log('path', JSON.stringify(route.routeConfig.path));
+      // console.log('storedHandle', this.storedHandle);
+      if (route.routeConfig.path === '' && this.storedHandle !== null) {
+        // console.log('return', this.storedHandle);
+        // console.groupEnd();
+        return this.storedHandle;
       }
     }
-    return future.routeConfig === curr.routeConfig;
+    // console.log('return', null);
+    // console.groupEnd();
+    // return this.storedHandle;
+    return null;
+  }
+
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    // console.group('shouldDetach');
+    // console.log(route);
+    // if (route && route.routeConfig) {
+    //   console.log('path', JSON.stringify(route.routeConfig.path));
+    //   console.log('storedHandle', this.storedHandle);
+    // }
+    // console.log('reture', route && route.routeConfig.path === '');
+    // console.groupEnd();
+    return route && route.routeConfig.path === '';
+  }
+
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    // console.group('store');
+    // console.log(route);
+    // console.log(handle);
+    // if (route && route.routeConfig) {
+    //   console.log('path', JSON.stringify(route.routeConfig.path));
+    //   console.log('detachedRouteHandle', handle);
+    // }
+
+    this.storedHandle = handle;
+    // console.log('storedHandle', this.storedHandle);
+    // console.groupEnd();
   }
 
 }
