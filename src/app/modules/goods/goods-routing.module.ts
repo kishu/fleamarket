26377091ab60464a0f92d4d746e7b77dd64c2922
TH <kishu@navercorp.com>
2019-01-26 14:20:29 +0900

    refactor(goods): router rules

--- src/app/modules/goods/goods-routing.module.ts
+++ src/app/modules/goods/goods-routing.module.ts
@@ -3,25 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../../shared/guards';
 import { GoodsGuard } from './goods.guard';
 import { GoodsAuthorityGuard } from './goods-authority-guard.service';
-import { WriteComponent } from './write/write.component';
 import { EditComponent } from './edit/edit.component';
 import { DetailComponent } from './detail/detail.component';
 
-
 const routes: Routes = [
   {
-    path: 'markets/:market/goods',
+    path: 'goods',
     canActivate: [AuthGuard],
     children: [
-      // {path: 'write', component: WriteComponent},
       { path: ':goodsId', canActivate: [ GoodsGuard ], component: DetailComponent },
       { path: ':goodsId/edit', canActivate: [ GoodsAuthorityGuard ], component: EditComponent }
     ]
   }
 ];
-
-// https://2ndmarket.co/market/webtoons/goods/
-// https://2ndmarket.co/market/lounge/goods/
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
