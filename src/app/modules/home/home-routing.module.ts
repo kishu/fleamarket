27425665ac4560f1a8c105b315218716def5edfc
TH <kishu@navercorp.com>
2019-01-26 14:20:17 +0900

    refactor(home): home matcher

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, UrlSegment } from '@angular/router';
 import { AuthGuard } from '../../shared/guards';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+
+export function homeMatcher(url: UrlSegment[]) {
+  if (url.length === 0 || (url.length === 1 && url[0].path === 'lounge')) {
+    return {consumed: url};
+  } else {
+    return null;
+  }
+}
 
 const routes: Routes = [
   {
-    path: '', canActivate: [AuthGuard],
+    matcher: homeMatcher,
+    canActivate: [AuthGuard],
     component: HomeComponent
   }
 ];
