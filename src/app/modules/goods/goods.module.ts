73f974ac56959a29f83ae4745a90fa3cff6a4eab
TH <kishu@navercorp.com>
2019-01-26 16:15:21 +0900

    refactor(goods) remove write component

--- src/app/modules/goods/goods.module.ts
+++ src/app/modules/goods/goods.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { TargetSelectedValidatorDirective } from './target-selected-validator.directive';
 import { GoodsRoutingModule } from './goods-routing.module';
-import { WriteComponent } from './write/write.component';
 import { FileUploadService } from '../../core/http';
 import { DetailComponent } from './detail/detail.component';
 import { EditComponent } from './edit/edit.component';
@@ -12,7 +11,6 @@ import { EditComponent } from './edit/edit.component';
 @NgModule({
   declarations: [
     TargetSelectedValidatorDirective,
-    WriteComponent,
     DetailComponent,
     EditComponent
   ],
