be879dc49eb55e85f3bbc10a5f7449b6958da692
TH <kishu@navercorp.com>
2019-02-03 10:55:28 +0900

    fixed(goods-edit): temporarily disable image rotate

--- src/app/shared/models/image-file.ts
+++ src/app/shared/models/image-file.ts
@@ -1,8 +1,8 @@
-import * as EXIF from 'exif-js';
+// import * as EXIF from 'exif-js';
 
 export class ImageFile {
   private readonly _file: File;
-  private _url: string;
+  private _url: ArrayBuffer;
 
   get file() { return this._file; }
   get url() { return this._url; }
@@ -16,9 +16,10 @@ export class ImageFile {
       const reader = new FileReader();
 
       reader.addEventListener('load', async () => {
-        const orientation = await this.getImageOrientation(this.file);
-        const refineImageUrl = await this.refineImage(reader.result, orientation);
-        this._url = refineImageUrl;
+        // const orientation = await this.getImageOrientation(this.file);
+        // const refineImageUrl = await this.refineImage(reader.result, orientation);
+        // this._url = refineImageUrl;
+        this._url = reader.result as ArrayBuffer;
         resolve();
       }, false);
 
@@ -26,53 +27,53 @@ export class ImageFile {
     });
   }
 
-  private getImageOrientation(file) {
-    return new Promise((resolve, reject) => {
-      try {
-        EXIF.getData(file, () => {
-          const orientation = EXIF.getTag(file, 'Orientation') || 0;
-          resolve(orientation);
-        });
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
+  // private getImageOrientation(file) {
+  //   return new Promise((resolve, reject) => {
+  //     try {
+  //       EXIF.getData(file, () => {
+  //         const orientation = EXIF.getTag(file, 'Orientation') || 0;
+  //         resolve(orientation);
+  //       });
+  //     } catch (e) {
+  //       reject(e);
+  //     }
+  //   });
+  // }
 
-  private refineImage(dataUrl, srcOrientation): Promise<string> {
-    return new Promise(async (resolve) => {
-      const img = new Image();
-
-      img.onload = function() {
-        const width = img.width;
-        const height = img.height;
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-
-        if (4 < srcOrientation && srcOrientation < 9) {
-          canvas.width = height;
-          canvas.height = width;
-        } else {
-          canvas.width = width;
-          canvas.height = height;
-        }
-
-        switch (srcOrientation) {
-          case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
-          case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
-          case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
-          case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-          case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
-          case 7: ctx.transform(0, -1, -1, 0, height , width); break;
-          case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
-          default: break;
-        }
-
-        ctx.drawImage(img, 0, 0);
-        resolve(canvas.toDataURL());
-      };
-
-      img.src = dataUrl;
-    });
-  }
+  // private refineImage(dataUrl, srcOrientation): Promise<string> {
+  //   return new Promise(async (resolve) => {
+  //     const img = new Image();
+  //
+  //     img.onload = function() {
+  //       const width = img.width;
+  //       const height = img.height;
+  //       const canvas = document.createElement('canvas');
+  //       const ctx = canvas.getContext('2d');
+  //
+  //       if (4 < srcOrientation && srcOrientation < 9) {
+  //         canvas.width = height;
+  //         canvas.height = width;
+  //       } else {
+  //         canvas.width = width;
+  //         canvas.height = height;
+  //       }
+  //
+  //       switch (srcOrientation) {
+  //         case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+  //         case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+  //         case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+  //         case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+  //         case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+  //         case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+  //         case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+  //         default: break;
+  //       }
+  //
+  //       ctx.drawImage(img, 0, 0);
+  //       resolve(canvas.toDataURL());
+  //     };
+  //
+  //     img.src = dataUrl;
+  //   });
+  // }
 }
