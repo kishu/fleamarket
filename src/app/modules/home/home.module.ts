e17156eaf0532ae9e7934c800520056807421ec8
TH <kishu@navercorp.com>
2019-02-10 13:47:28 +0900

    refactor: remove reuse-strategy

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouteReuseStrategy } from '@angular/router';
-import { CustomReuseStrategy } from '@app/modules/home/custom-reuse-strategy';
 import { HomeRoutingModule } from '@app/modules/home/home-routing.module';
 import { HomeComponent } from '@app/modules/home/home/home.component';
 
@@ -12,9 +10,6 @@ import { HomeComponent } from '@app/modules/home/home/home.component';
   imports: [
     CommonModule,
     HomeRoutingModule
-  ],
-  providers: [
-    // { provide: RouteReuseStrategy, useClass: CustomReuseStrategy }
   ],
   exports: [
     HomeComponent
