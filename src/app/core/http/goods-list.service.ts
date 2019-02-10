56fcfb8d9a2d4765b7b9d5b5a0c03cd7a97a0735
TH <kishu@navercorp.com>
2019-02-10 12:55:18 +0900

    refactor: goods-list service

--- src/app/core/http/goods-list.service.ts
+++ src/app/core/http/goods-list.service.ts
@@ -1,91 +1,77 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { filter, map, switchMap, tap } from 'rxjs/operators';
-import { LoggedIn } from '@app/core/logged-in.service';
+import { first, map } from 'rxjs/operators';
 import { FirebaseUtilService, FirebaseQueryBuilderOptions } from '@app/shared/services';
 import { Goods } from '@app/shared/models';
-
-export interface GoodsListFilter {
-  market: string;
-  exceptSoldOut: boolean;
-}
+import { Observable } from 'rxjs';
+import { LoggedIn } from '@app/core/logged-in.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoodsListService {
-  goodsList$ = new BehaviorSubject<Goods[]>([]);
-  private goodsListFilter: GoodsListFilter;
-  private goodsListFilter$: Subject<GoodsListFilter> = new Subject();
 
   constructor(
     private afs: AngularFirestore,
     private loggedIn: LoggedIn
-  ) {
-    this.goodsListFilter$.pipe(
-      filter(goodsListfilter => !!goodsListfilter),
-      tap(goodsListfilter => this.goodsListFilter = goodsListfilter),
-      switchMap(goodsListFilter => {
-        if (goodsListFilter.market === 'group') {
-          return this.getGoods$ByGroup(this.loggedIn.user.groupRef, goodsListFilter.exceptSoldOut);
-        } else if (goodsListFilter.market === 'lounge') {
-          return this.getGoods$ByLounge(goodsListFilter.exceptSoldOut);
-        }
-      })
-    ).subscribe(goodsList => {
-      this.goodsList$.next(goodsList);
-    });
-  }
+  ) { }
 
-  updateGoodsListFilter(goodsListFilter: GoodsListFilter) {
-    if (!Object.is(this.goodsListFilter, goodsListFilter)) {
-      this.goodsListFilter$.next(goodsListFilter);
+  getGoodsListBy(market: string, exceptSoldOut: boolean): Observable<Goods[]> {
+    if (market === 'group') {
+      return this.getGoodsListByGroup(this.loggedIn.user.groupRef, exceptSoldOut);
+    } else if (market === 'lounge') {
+      return this.getGoodsListByLounge(exceptSoldOut);
     }
   }
 
-  private getGoods$ByGroup(groupRef: DocumentReference, exceptSoldout: boolean) {
+  getGoodsListByGroup(groupRef: DocumentReference, exceptSoldOut: boolean): Observable<Goods[]> {
     const options: FirebaseQueryBuilderOptions = {
       where: [
         ['groupRef', '==', groupRef],
         ['market.group', '==', true],
+        exceptSoldOut ? ['soldout', '==', false] : undefined
       ],
       orderBy: [
         ['updated', 'desc']
       ]
     };
-
-    if (exceptSoldout) {
-      options.where.push(['soldout', '==', false]);
-    }
-
-    return this.getGoods$(options);
+    return this.getGoodsList(options);
   }
 
-  private getGoods$ByLounge(exceptSoldout: boolean) {
+  getGoodsListByLounge(exceptSoldOut: boolean): Observable<Goods[]> {
     const options: FirebaseQueryBuilderOptions = {
       where: [
         ['market.lounge', '==', true],
+        exceptSoldOut ? ['soldout', '==', false] : undefined
       ],
       orderBy: [
         ['updated', 'desc']
       ]
     };
-
-    if (exceptSoldout) {
-      options.where.push(['soldout', '==', false]);
-    }
-
-    return this.getGoods$(options);
+    return this.getGoodsList(options);
   }
 
-  private getGoods$(options: FirebaseQueryBuilderOptions) {
+  getGoodsList(options: FirebaseQueryBuilderOptions): Observable<Goods[]> {
     return this.afs.collection(
       'goods',
       ref => FirebaseUtilService.buildQuery(ref, options)
     ).snapshotChanges().pipe(
+      first(),
       map(FirebaseUtilService.sirializeDocumentChangeActions)
     );
+  }
+
+  getGoodsListByUser(userRef: DocumentReference, market: string): Observable<Goods[]> {
+    const options: FirebaseQueryBuilderOptions = {
+      where: [
+        ['userRef', '==', userRef],
+        [`market.${market}`, '==', true]
+      ],
+      orderBy: [
+        ['updated', 'desc']
+      ]
+    };
+    return this.getGoodsList(options);
   }
 
 }
