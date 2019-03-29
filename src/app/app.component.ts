fb27f2e4209e753cfe1405da72b28528b0f4647f
sogoonii <sogoonii@naver.com>
2019-03-29 15:49:39 +0900

    feat(auth): review plz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { GlobalToggleService, HtmlClassService } from '@app/shared/services';
+import { AuthService } from '@app/core/http';
+import { User } from '@app/core/models';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,14 @@ import { GlobalToggleService, HtmlClassService } from '@app/shared/services';
 export class AppComponent {
   title = '2nd Market';
   isShowNotification = false;
+  user: User;
 
   constructor(
     private globalToggleService: GlobalToggleService,
-    private htmlClassService: HtmlClassService
+    private authService: AuthService,
+    private htmlClassService: HtmlClassService,
   ) {
+    this.user = this.authService.user;
     this.globalToggleService.notification$.asObservable().subscribe(isShow => {
       if (isShow === undefined) {
         this.isShowNotification = !this.isShowNotification;
